93df6c39bac1fb73b3300523151dcfd630b0f760
Anindit <5239439+aninditkarmakar@users.noreply.github.com>
2025-05-25 08:33:58 +0000

    tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,38 @@
+"use client";
+import { useState } from "react";
+
+import "./global.css";
+
 export default function Page() {
-  return <h1>Hello there, Next.js!</h1>;
+  const [videoSrc, setVideoSrc] = useState<string | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setVideoSrc(url);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="underline">Welcome to WatchParty!</h1>
+      <input
+        type="file"
+        accept="video/*"
+        onChange={handleFileChange}
+        style={{ margin: "20px 0" }}
+        className="file:mr-4 file:rounded-full file:border-0 file:bg-violet-50 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-violet-700 hover:file:bg-violet-100 dark:file:bg-violet-600 dark:file:text-violet-100 dark:hover:file:bg-violet-500"
+      />
+      {videoSrc && (
+        <video
+          controls
+          src={videoSrc}
+          style={{ width: "100%", maxHeight: "500px" }}
+        >
+          Your browser does not support the video tag.
+        </video>
+      )}
+    </div>
+  );
 }
